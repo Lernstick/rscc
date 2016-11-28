8c4eb3d5f2b752b2c721087eee031fa77804cdc5
Roger Obrist <roger.obrist@students.fhnw.ch>
2016-11-28 16:11:29 +0100

    Added ProvideSupport fxml and controller with basic function

---
+++ src/ch/imedias/rscc/controller/ProvideSupportController.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ch.imedias.rscc.controller;
+
+import ch.imedias.rscc.util.FXMLGuiLoader;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+/**
+ * FXML Controller class
+ *
+ * @author user
+ */
+public class ProvideSupportController implements Initializable {
+
+    @FXML
+    private ComboBox<Integer> cboCompression;
+    @FXML
+    private CheckBox chk8BitColor;
+    @FXML
+    private ComboBox<Integer> cboQuality;
+    @FXML
+    private TextField txtSafePorts;
+    @FXML
+    private CheckBox chkHttpsPort;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        cboCompression.getItems().addAll(1,2,3,4,5,6,7,8,9);
+        // TODO: set value from model
+        cboQuality.getItems().addAll(1,2,3,4,5,6,7,8,9);
+        // TODO: set value from model
+    }    
+
+    @FXML
+    private void onBackAction(ActionEvent event) {
+        Scene scene = ((Node)event.getSource()).getScene();
+        Stage stage = (Stage)scene.getWindow();
+        stage.setScene(FXMLGuiLoader.getInstance().getRemoteSupportStart());
+    }
+
+    @FXML
+    private void onStartServiceAction(ActionEvent event) {
+        
+    }
+    
+}
