4b21b17427ddb14ee0da6480a2e129ff3c9ea5d1
Debian Live user <user@localhost.localdomain>
2016-11-28 14:27:18 +0100

    extract settings to model

---
+++ src/Settings.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author patric steiner
+ */
+public class Settings {
+    private String securePorts;
+    private int quality;
+    private int compressionLevel;
+    private boolean bgr233;
+    
+}
