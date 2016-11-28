f696ec00f35e4d3ae95c38f114be56a09fec4479
LineStettler <user@localhost.localdomain>
2016-11-28 16:00:54 +0100

    changed EditDialogController to main Application for testing

--- src/ch/imedias/rscc/EditDialogController.java
+++ src/ch/imedias/rscc/EditDialogController.java
@@ -8,27 +8,33 @@ package ch.imedias.rscc;
 import java.net.URL;
 import java.util.ArrayList;
 import static java.util.Collections.swap;
+import java.util.List;
+import java.util.Map;
 import java.util.ResourceBundle;
-import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
-import javafx.scene.input.MouseEvent;
+import java.util.logging.Logger;
+import javafx.application.Application;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import java.util.logging.Logger;
-import java.util.Map;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.SelectionMode;
 import javafx.scene.control.SelectionModel;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+import javafx.fxml.FXMLLoader;
 
 /**
  * FXML Controller class
  *
  * @author user
  */
-public class EditDialogController implements Initializable {
+public class EditDialogController extends Application implements Initializable {
 
     @FXML
     private Button CancelButton;
@@ -50,28 +56,44 @@ public class EditDialogController implements Initializable {
     private final static Logger LOGGER
             = Logger.getLogger(EditDialog.class.getName());
     
-    private final SupportAddressesTableModel tableModel;
+    //private final SupportAddressesTableModel tableModel;
     
     private ObservableList<SupportAddress> supportAddresses;
     private boolean okPressed;
     private SelectionModel<SupportAddress> selectionModel;
     @FXML
     private TableView<SupportAddress> table;
-    @FXML
-    private TableColumn<Map, String> name;
-    @FXML
-    private TableColumn<Map, String> address;
-    @FXML
-    private TableColumn<Map, Boolean> encrypted;
+  //  @FXML
+  //  private TableColumn<Map, String> name;
+  //  @FXML
+  //  private TableColumn<Map, String> address;
+   // @FXML
+   // private TableColumn<Map, Boolean> encrypted;
     
     
+    private List<SupportAddress> staticDefaultAddressList;
+    private List<SupportAddress> staticAddressList;
 
+    
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
+        System.out.println("ch.imedias.rscc.EditDialogController.initialize()");
+        staticAddressList = new ArrayList();
+        staticDefaultAddressList = new ArrayList();
 
+        
+        staticAddressList.add(new SupportAddress("Hallo", "hallo.hallo", false));
+        staticAddressList.add(new SupportAddress("Fritz", "fritz.hallo", true));
+        staticAddressList.add(new SupportAddress("Meier", "Meier.hallo", false));
+
+        staticDefaultAddressList.add(new SupportAddress("eins", "eins.hallo", false));
+        staticDefaultAddressList.add(new SupportAddress("zwei", "zwei.hallo", true));
+        staticDefaultAddressList.add(new SupportAddress("drei", "drei.hallo", false));
+        
+        
         // make a deep copy of the list
         this.supportAddresses = FXCollections.observableArrayList();
         for (SupportAddress supportAddress : staticAddressList) {
@@ -83,15 +105,16 @@ public class EditDialogController implements Initializable {
             this.supportAddresses.add(addressCopy);
             }
         }
-        this.selectionModel = table.getSelectionModel();
-        table.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
+        //this.selectionModel = table.getSelectionModel();
+        //table.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
         
-        tableModel = new SupportAddressesTableModel();
+        //tableModel = new SupportAddressesTableModel();
         //initComponents();
-        table.setModel(tableModel);
+        //table.setModel(tableModel);
         
         
         table.itemsProperty().setValue(supportAddresses);
+        
        // table.setEditable(true);
         //table.
         // determine size and location
@@ -165,6 +188,35 @@ public class EditDialogController implements Initializable {
     @FXML
     private void selectAddress(MouseEvent event) {
 
+    }
+
+    @Override
+    public void start(Stage mainStage) throws Exception {
+
+        Parent root = FXMLLoader.load(getClass().getResource("editDialog.fxml"));
+        Scene myScene = new Scene(root); 
+        mainStage.setScene(myScene);
+        mainStage.setMinWidth(300);
+        mainStage.setMinHeight(300); 
+        mainStage.show();
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+        /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+      /*  EventQueue.invokeLater(new Runnable() {
+
+            @Override
+            public void run() {
+                RemoteSupportFrame frame = new RemoteSupportFrame();
+                SEEK_PROCESS_EXECUTOR.addPropertyChangeListener(frame);
+                frame.setVisible(true);
+            }
+        
+        });*/
+      launch(args);
     }
     
 }
